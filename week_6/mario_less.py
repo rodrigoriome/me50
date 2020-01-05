fc2ded336c84371685c0a1d8759b6ecb9d2eb1eb
Rodrigo Mesquita <rodrigoriome@gmail.com>
2020-01-05 12:23:58 -0300

    finish week_6/mario(less)

---
+++ week_6/mario_less.py
@@ -0,0 +1,13 @@
+from cs50 import get_int
+
+while True:
+    size = get_int("Height: ")
+    if size > 0 and size < 9: break
+
+for i in range(size):
+    for j in range(size):
+        if j >= size - i - 1:
+            print("#", end="")
+        else:
+            print(" ", end="")
+    print()
