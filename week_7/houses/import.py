a5c466fadaf944a3f1bd8e27a0882eb03185b11d
Rodrigo Mesquita <rodrigoriome@gmail.com>
2020-01-08 07:34:40 -0300

    finish week_7/houses

--- week_7/houses/import.py
+++ week_7/houses/import.py
@@ -1,1 +1,17 @@
-# TODO
+from sys import argv, exit
+from cs50 import SQL
+import csv
+
+if len(argv) != 2:
+    print("Please provide a valid spreadsheet")
+    exit(1)
+
+db = SQL("sqlite:///students.db")
+
+with open(argv[1], "r") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        names = row['name'].split(' ')
+        if len(names) == 2:
+            names.insert(1, None)
+        db.execute("INSERT INTO students (first, middle, last, house, birth) values (?, ?, ?, ?, ?)", names[0], names[1], names[2], row["house"], row["birth"])
--- week_7/houses/roster.py
+++ week_7/houses/roster.py
@@ -1,1 +1,13 @@
-# TODO
+from sys import argv, exit
+from cs50 import SQL
+
+if len(argv) != 2:
+    print("Please enter a house name")
+    exit(1)
+
+db = SQL("sqlite:///students.db")
+query = db.execute("SELECT * FROM students WHERE house = ? ORDER BY last, first", argv[1])
+
+for row in query:
+    middle = " " + row['middle'] if row['middle'] else ""
+    print(f"{row['first']}{middle} {row['last']}, born {row['birth']}")
